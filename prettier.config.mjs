b8535cd3746f3a28d35c97b7a8132b193f2aaec0
Cosmin Nicolaescu <cos@cosn.dev>
2024-06-16 15:56:49 +0100

    add prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+export const config = {
+  arrowParens: 'always',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx', 'cn'],
+}
+
+export default config
