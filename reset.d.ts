180e43d7e4511eefcf3df5644b63ba7632a9132f
Cosmin Nicolaescu <cos@accrual.com>
2024-11-04 11:24:48 -0800

    fix: handle css and svg

--- reset.d.ts
+++ reset.d.ts
@@ -1,1 +1,11 @@
 import '@total-typescript/ts-reset/dom'
+
+declare module '*.css?url' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.svg?react' {
+  const content: any;
+  export default content;
+}
