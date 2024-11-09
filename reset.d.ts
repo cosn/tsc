bd448b86a8b5df2b3c589fce1db36c0a6fdb2d33
Cosmin Nicolaescu <cos@accrual.com>
2024-11-09 14:01:51 -0800

    style: format

--- reset.d.ts
+++ reset.d.ts
@@ -1,11 +1,11 @@
 import '@total-typescript/ts-reset/dom'
 
 declare module '*.css?url' {
-  const content: any;
-  export default content;
+  const content: any
+  export default content
 }
 
 declare module '*.svg?react' {
-  const content: any;
-  export default content;
+  const content: any
+  export default content
 }
